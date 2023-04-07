7fc70ac88a57290b6b22c7097083d06b85a1ad29
PR0C3S <20180767@ce.pucmm.edu.do>
2023-04-07 17:56:26 -0400

    remove elements

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import Gretting from './components/pure/gretting';
-import GreetingF from './components/pure/greetingF';
-import TaskListComponent from './components/container/task_list';
 import ContactListComponent from './components/container/contact_list';
 
 function App() {
@@ -10,12 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {/* Componente propio Greeting.jsx
-        <Gretting name="John Pena"></Gretting>
-        
-        <GreetingF name="Harold Pena"></GreetingF>
-        
-        <TaskListComponent></TaskListComponent>*/}
+        {/* */}
         <ContactListComponent></ContactListComponent>
       </header>
 
