806f10ab0cece196d83be24a311fba0f6d49261d
PR0C3S <20180767@ce.pucmm.edu.do>
2023-04-07 17:53:39 -0400

    remove elements

--- src/App.js
+++ src/App.js
@@ -1,13 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import Gretting from './components/pure/gretting';
-import GreetingF from './components/pure/greetingF';
-import TaskListComponent from './components/container/task_list';
-import Ejemplo1 from './hooks/ejemplo1';
-import Ejemplo2 from './hooks/ejemplo2';
-import MiComponenteConContexto from './hooks/ejemplo3';
-import Ejemplo4 from './hooks/ejemplo4';
-import GreetingStyled from './components/pure/greetingStyled';
 import Clock from './components/pure/clock';
 import ClockHook from './hooks/clockHook';
 
@@ -16,30 +8,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        {/* Componente propio Greeting.jsx
-        <Gretting name="John Pena"></Gretting>
-        
-        <GreetingF name="Harold Pena"></GreetingF>
-        
-       
-        Ejemplo de uso de HOOKS
-        <Ejemplo1></Ejemplo1>
-        <Ejemplo2></Ejemplo2>
-        <MiComponenteConContexto></MiComponenteConContexto>
-        */}
-        
-        
-       {/* <Ejemplo4 nombre='John'>}
-           Todo lo que esta aqui adentro es tratado como props.children*/ }
-          {/*<h3>
-            Contenido del props.children
-          </h3>
-        </Ejemplo4>
-       
-       <TaskListComponent></TaskListComponent>
-       
-       <GreetingStyled name='John'></GreetingStyled>
-       
+        {/* 
        <Clock></Clock>
        */}
        <ClockHook></ClockHook>
